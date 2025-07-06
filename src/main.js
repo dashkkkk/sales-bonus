6e138ae27fad01449810b6809bf513de888f46b4
dasha <luakhdaria@gmail.com>
2025-07-06 19:26:07 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ function calculateBonusByProfit(index, total, seller) {
     return Math.round(bonus * 100) / 100;
 }
 
-function analyzeSalesData(data, options) {  
+function analyzeSalesData(data, options) { 
     if (!data || !Array.isArray(data.sellers) || 
         !Array.isArray(data.purchase_records) || 
         !Array.isArray(data.products) ||
@@ -46,13 +46,15 @@ function analyzeSalesData(data, options) {
         throw new Error('Некорректные входные данные');
     }
     
-  
-    if (arguments.length < 2 || typeof options !== 'object' || Array.isArray(options)) {
+    
+    if (!options || typeof options !== 'object' || Array.isArray(options) ||
+        (options.calculateRevenue && !options.calculateBonus) ||
+        (options.calculateBonus && !options.calculateRevenue)) {
         throw new Error('Некорректные входные данные');
     }
 
-    const calculateRevenue = options.calculateRevenue || calculateSimpleRevenue;
-    const calculateBonus = options.calculateBonus || calculateBonusByProfit;
+    const calculateRevenue = options?.calculateRevenue || calculateSimpleRevenue;
+    const calculateBonus = options?.calculateBonus || calculateBonusByProfit;
 
     if (typeof calculateRevenue !== 'function' || typeof calculateBonus !== 'function') {
         throw new Error('Некорректные входные данные');
